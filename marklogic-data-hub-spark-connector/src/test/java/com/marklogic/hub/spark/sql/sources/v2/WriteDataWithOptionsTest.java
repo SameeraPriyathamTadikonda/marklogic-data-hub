489e0c03cf649aaa9b671aa86c59e001d51b17d5
Rob Rudin <rob.rudin@marklogic.com>
2020-10-05 10:51:30 -0400

    DHFPROD-5944: Adding helper methods for test

--- marklogic-data-hub-spark-connector/src/test/java/com/marklogic/hub/spark/sql/sources/v2/WriteDataWithOptionsTest.java
+++ marklogic-data-hub-spark-connector/src/test/java/com/marklogic/hub/spark/sql/sources/v2/WriteDataWithOptionsTest.java
@@ -1,89 +1,72 @@
 package com.marklogic.hub.spark.sql.sources.v2;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.marklogic.client.document.JSONDocumentManager;
-import com.marklogic.client.eval.EvalResultIterator;
-import com.marklogic.client.ext.util.DefaultDocumentPermissionsParser;
-import com.marklogic.client.ext.util.DocumentPermissionsParser;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.JacksonHandle;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.sources.v2.writer.DataWriter;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WriteDataWithOptionsTest extends AbstractSparkConnectorTest {
 
     @Test
     void ingestDocsWithCollection() throws IOException {
-        String collections = "fruits";
-        DataWriter<InternalRow> dataWriter = buildDataWriter(new Options(getHubPropertiesAsMap()).withBatchSize(1).withUriPrefix("/testFruit").withCollections(collections));
+        String collections = "fruits,stuff";
+        DataWriter<InternalRow> dataWriter = buildDataWriter(newFruitOptions().withCollections(collections));
         dataWriter.write(buildRow("pineapple", "green"));
-        String uriQuery = "cts.uriMatch('/testFruit**')";
 
-        EvalResultIterator uriQueryResult = getHubClient().getStagingClient().newServerEval().javascript(uriQuery).eval();
-        String uri = uriQueryResult.next().getString();
+        DocumentMetadataHandle metadata = getFirstFruitMetadata();
+        assertEquals(2, metadata.getCollections().size());
+        assertTrue(metadata.getCollections().contains("fruits"));
+        assertTrue(metadata.getCollections().contains("stuff"));
 
-        JSONDocumentManager jd = getHubClient().getStagingClient().newJSONDocumentManager();
-        DocumentMetadataHandle docMetaData = jd.readMetadata(uri, new DocumentMetadataHandle());
-
-        assertEquals(collections, docMetaData.getCollections().toArray()[0].toString());
+        // Verify default permissions
+        DocumentMetadataHandle.DocumentPermissions perms = getFirstFruitMetadata().getPermissions();
+        Set<DocumentMetadataHandle.Capability> capabilities = perms.get("data-hub-common");
+        assertTrue(capabilities.contains(DocumentMetadataHandle.Capability.READ));
+        assertTrue(capabilities.contains(DocumentMetadataHandle.Capability.UPDATE));
     }
 
     @Test
     void ingestDocsWithSourceName() throws IOException {
         String sourceName = "spark";
-        DataWriter<InternalRow> dataWriter = buildDataWriter(new Options(getHubPropertiesAsMap()).withBatchSize(1).withUriPrefix("/testFruit").withSourceName(sourceName));
+        DataWriter<InternalRow> dataWriter = buildDataWriter(newFruitOptions().withSourceName(sourceName));
         dataWriter.write(buildRow("pineapple", "green"));
-        String uriQuery = "cts.uriMatch('/testFruit**')";
 
-        EvalResultIterator uriQueryResult = getHubClient().getStagingClient().newServerEval().javascript(uriQuery).eval();
-        String uri = uriQueryResult.next().getString();
-
-        JSONDocumentManager jd = getHubClient().getStagingClient().newJSONDocumentManager();
-        JsonNode doc = jd.read(uri, new JacksonHandle()).get();
-
+        JsonNode doc = getHubClient().getStagingClient().newJSONDocumentManager().read(getFruitUris()[0], new JacksonHandle()).get();
         assertEquals(sourceName, doc.get("envelope").get("headers").get("sources").get(0).get("name").asText());
     }
 
     @Test
     void ingestDocsWithSourceType() throws IOException {
         String sourceType = "fruits";
-        DataWriter<InternalRow> dataWriter = buildDataWriter(new Options(getHubPropertiesAsMap()).withBatchSize(1).withUriPrefix("/testFruit").withSourceType(sourceType));
+        DataWriter<InternalRow> dataWriter = buildDataWriter(newFruitOptions().withSourceType(sourceType));
         dataWriter.write(buildRow("pineapple", "green"));
-        String uriQuery = "cts.uriMatch('/testFruit**')";
 
-        EvalResultIterator uriQueryResult = getHubClient().getStagingClient().newServerEval().javascript(uriQuery).eval();
-        String uri = uriQueryResult.next().getString();
-
-        JSONDocumentManager jd = getHubClient().getStagingClient().newJSONDocumentManager();
-        JsonNode doc = jd.read(uri, new JacksonHandle()).get();
-
+        JsonNode doc = getHubClient().getStagingClient().newJSONDocumentManager().read(getFruitUris()[0], new JacksonHandle()).get();
         assertEquals(sourceType, doc.get("envelope").get("headers").get("sources").get(0).get("datahubSourceType").asText());
     }
 
     @Test
     void ingestDocsWithPermissions() throws IOException {
-        String permissions = "data-hub-common,read,data-hub-operator,update";
-        DataWriter<InternalRow> dataWriter = buildDataWriter(new Options(getHubPropertiesAsMap()).withBatchSize(1).withUriPrefix("/testFruit").withPermissions(permissions));
+        String permissions = "qconsole-user,read,manage-admin,update";
+        DataWriter<InternalRow> dataWriter = buildDataWriter(newFruitOptions().withPermissions(permissions));
         dataWriter.write(buildRow("pineapple", "green"));
-        String uriQuery = "cts.uriMatch('/testFruit**')";
 
-        EvalResultIterator uriQueryResult = getHubClient().getStagingClient().newServerEval().javascript(uriQuery).eval();
-        String uri = uriQueryResult.next().getString();
-
-        JSONDocumentManager jd = getHubClient().getStagingClient().newJSONDocumentManager();
-        DocumentMetadataHandle docMetaData = jd.readMetadata(uri, new DocumentMetadataHandle());
-
-        DocumentPermissionsParser documentPermissionParser = new DefaultDocumentPermissionsParser();
-        DocumentMetadataHandle permissionMetadata = new DocumentMetadataHandle();
-        documentPermissionParser.parsePermissions(permissions, permissionMetadata.getPermissions());
-
-        Assert.assertEquals(permissionMetadata.getPermissions(), docMetaData.getPermissions());
+        DocumentMetadataHandle.DocumentPermissions perms = getFirstFruitMetadata().getPermissions();
+        assertEquals(DocumentMetadataHandle.Capability.READ, perms.get("qconsole-user").iterator().next());
+        assertEquals(DocumentMetadataHandle.Capability.UPDATE, perms.get("manage-admin").iterator().next());
     }
 
+    private DocumentMetadataHandle getFirstFruitMetadata() {
+        String[] uris = getFruitUris();
+        assertTrue(uris.length > 0, "Expected at least one fruit URI, found zero");
+        return getHubClient().getStagingClient().newDocumentManager().readMetadata(uris[0], new DocumentMetadataHandle());
+    }
 }
